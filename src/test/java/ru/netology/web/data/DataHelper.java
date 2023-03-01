b96efa7580a3ad480386590dd48bb0581abc7f04
sanyaminkin <sanyaminkin@gmail.com>
2023-03-01 22:21:15 +0300

    DataHelper.java and DashboardPage.java and MoneyTransferTest.java

--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -3,7 +3,8 @@ package ru.netology.web.data;
 import lombok.Value;
 
 public class DataHelper {
-    private DataHelper() {}
+    private DataHelper() {
+    }
 
     @Value
     public static class AuthInfo {
@@ -26,5 +27,18 @@ public class DataHelper {
 
     public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
         return new VerificationCode("12345");
+    }
+
+    @Value
+    public static class CardNumber {
+        private String cardNumber;
+
+        public static CardNumber getFirstCardNumber() {
+            return new CardNumber("5559 0000 0000 0001");
+        }
+
+        public static CardNumber getSecondCardNumber() {
+            return new CardNumber("5559 0000 0000 0002");
+        }
     }
 }
--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -7,6 +7,16 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class DashboardPage {
     private SelenideElement heading = $("[data-test-id=dashboard]");
+    private SelenideElement topUpFirstCardButton = $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']")
+            .$("[data-test-id='action-deposit']");
+    private SelenideElement topUpSecondCardButton = $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']")
+            .$("[data-test-id='action-deposit']");
+    private SelenideElement reloadButton = $("[data-test-id='action-reload']");
+    private SelenideElement transactionAmountField = $("[data-test-id='amount'] input");
+    private SelenideElement transactionFromCardField = $("[data-test-id='from'] input");
+    private SelenideElement transactionToCardField = $("[data-test-id='to']");
+    private SelenideElement topUpActionButton = $("[data-test-id='action-transfer']");
+    private SelenideElement topUpCancelButton = $("[data-test-id='action-cancel']");
 
     public DashboardPage() {
         heading.shouldBe(visible);
